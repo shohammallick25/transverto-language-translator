f303234c0f88142e97a589cf90100b4f8341d6c4
shohammallick25 <shohammallick528@gmail.com>
2025-07-16 19:03:16 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,21 @@
-const CACHE_NAME = "transverto-cache-v1";
-const ASSETS_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./languages.js",
-  "./manifest.json",
-  "./images/logo.png",
-  "./images/sun.png",
-  "./images/moon.png",
-  "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js",
-  "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+const CACHE_NAME = 'transverto-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/languages.js',
+  '/images/logo.png'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
-});
-
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
